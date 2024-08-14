9c40b60258332cb805e952be7016b8a7a9c36143
vivek-sps <vivek_sharma@softprodigy.com>
2024-08-14 16:14:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,6 @@ if ingredients_list:
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+# st.text(fruityvice_response)
 
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
