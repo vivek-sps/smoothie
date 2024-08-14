6439145dfb2000bb2458c636256c3ccc3de6cc4c
vivek-sps <vivek_sharma@softprodigy.com>
2024-08-14 17:06:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ if ingredients_list:
         st.write('The search value for ', x,' is ', search_on, '.')
         
         st.subheader(x + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + x)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
